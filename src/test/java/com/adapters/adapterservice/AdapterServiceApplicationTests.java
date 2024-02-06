4c8009b4dd065797f4e683599e31527933088b70
anton <antonkuchisnky86@gmail.com>
2024-02-06 13:46:21 +0300

    Initial commit

---
+++ src/test/java/com/adapters/adapterservice/AdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adapters.adapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
